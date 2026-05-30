c520d6a4acc45c99a041f79dd22c8537497e05f7
shani5589 <safdarshani723327@gmail.com>
2026-05-30 14:12:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -371,7 +371,7 @@ def __2010___2011():
         data = random.choice(["100001","100002","100003","100004"])+str(random.choice(range(111111111, 999999999)))
         user.append(data)
     with ShaniXD(max_workers=50) as Shani:
-        tl = str(limit)
+        total_ids = int(limit)
         clear()
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
@@ -398,7 +398,7 @@ def ____old2009___():
 
     with ShaniXD(max_workers=30) as Shani:
         clear()
-        tl = str(len(user))
+        total_ids = len(user)
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m TOTAL IDS : \033[92m{tl}")
         print(f"\033[1;90m⌠\033[1;97m=\033[1;90m⌡\033[0;97m USE 1.1.1.1 VPN FOR BEST RESULT")
         linex()
